3a4001ccbc25cb9fc50d1b00691f68a289eef19d
Vlad Bologa <vlad.bologa@gmail.com>
2019-11-25 00:11:39 +0100

    Add some basic tests for vectors and points

---
+++ RayTracer/RayTracerLib/Math.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace RayTracer {
+
+Eigen::Vector4f Vector(float x, float y, float z) {
+	return Eigen::Vector4f(x, y, z, 0.0f);
+}
+
+Eigen::Vector4f Point(float x, float y, float z) {
+	return Eigen::Vector4f(x, y, z, 1.0f);
+}
+
+}
