c838f6e84bc44b614f1a7ffc356aa91db8e25b44
Vlad Bologa <vlad.bologa@gmail.com>
2026-09-13 13:36:32 +0200

    Fix eigen import

--- RayTracer/cmake/AddEigen.cmake
+++ RayTracer/cmake/AddEigen.cmake
@@ -6,11 +6,4 @@ FetchContent_Declare(
     GIT_TAG        5.0.1
     GIT_SHALLOW    TRUE)
 
-FetchContent_GetProperties(Eigen3)
-if(NOT eigen3_POPULATED)
-    FetchContent_Populate(Eigen3)
-
-    add_library(Eigen3_Eigen INTERFACE)
-    target_include_directories(Eigen3_Eigen SYSTEM INTERFACE "${eigen3_SOURCE_DIR}")
-    add_library(Eigen3::Eigen ALIAS Eigen3_Eigen)
-endif()
+FetchContent_MakeAvailable(Eigen3)
