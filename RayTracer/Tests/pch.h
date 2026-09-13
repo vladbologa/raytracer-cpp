5147d438c9d098d06708b821a693c26688d41b39
Vlad Bologa <vlad.bologa@gmail.com>
2026-09-13 21:13:37 +0200

    fix pch

--- RayTracer/Tests/pch.h
+++ RayTracer/Tests/pch.h
@@ -5,9 +5,17 @@
 
 #pragma once
 
+#ifdef _MSC_VER
 #pragma warning(push, 0)
+#endif
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 #include "Eigen/Dense"
+
+#ifdef _MSC_VER
 #pragma warning(pop)
+#endif
+
+#include "Matchers.h"
