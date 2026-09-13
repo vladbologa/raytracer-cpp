3d04233097edc3037073f6a2b724751e1ecb7b82
Vlad Bologa <vlad.bologa@gmail.com>
2026-09-13 22:47:57 +0200

    Flatten project structure (#7)

--- cmake/AddGoogleTest.cmake
+++ cmake/AddGoogleTest.cmake
@@ -1,5 +1,8 @@
 include(FetchContent)
 
+# Must be set before population so GoogleTest's own CMakeLists.txt sees it.
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
 FetchContent_Declare(googletest
         GIT_REPOSITORY      https://github.com/google/googletest.git
         GIT_TAG             v1.17.0
@@ -7,8 +10,6 @@ FetchContent_Declare(googletest
 
 FetchContent_GetProperties(googletest)
 FetchContent_MakeAvailable(googletest)
-
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
 set_target_properties(gtest gtest_main gmock gmock_main
     PROPERTIES FOLDER "Extern")
