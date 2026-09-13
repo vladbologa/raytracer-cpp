c76de1d93966c46bc96c69953e448748ae37e64d
Vlad Bologa <vlad.bologa@gmail.com>
2026-09-13 21:31:44 +0200

    Remove inaccurate TODOs

--- RayTracer/Tests/Matchers.h
+++ RayTracer/Tests/Matchers.h
@@ -11,7 +11,6 @@ MATCHER_P2(IsSimilarToVector, expected, epsilon, "") {
         return false;
     }
 
-    // TODO: Eigen 3.4 will support STL iterators
     for (Eigen::Index i = 0; i < arg.size(); ++i) {
         if (std::abs(arg[i] - expected[i]) > epsilon) {
             *result_listener << "elements at index " << i << " are not similar: " << arg[i] << " "
@@ -30,7 +29,6 @@ MATCHER_P2(IsSimilarToMatrix, expected, epsilon, "") {
         return false;
     }
 
-    // TODO: Eigen 3.4 will support STL iterators
     for (Eigen::Index i = 0; i < arg.rows(); ++i) {
         const auto rowArg = arg.row(i);
         const auto rowExpected = expected.row(i);
